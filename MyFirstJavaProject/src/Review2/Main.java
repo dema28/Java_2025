9ef72474bd35781c5eb79394cc9ce00438a215e9
Denis Novicov <dema28ster@gmail.com>
2025-02-11 04:34:00 +0100

    Review02

---
+++ MyFirstJavaProject/src/Review2/Main.java
@@ -0,0 +1,17 @@
+package Review2;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 12;
+        boolean isEven  = (a % 2 == 0);
+        int result = 0;
+
+        System.out.println(result);
+        if (isEven) {
+            result = a * 3;
+        } else {
+            result = a * 2;
+        }
+        System.out.println(result);
+    }
+}
---
+++ MyFirstJavaProject/src/Review2/Main3.java
@@ -0,0 +1,12 @@
+package Review2;
+
+public class Main3 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i <= 10; i = i + 2) {
+            sum = sum + i;
+            }
+        System.out.println(sum);
+    }
+}
