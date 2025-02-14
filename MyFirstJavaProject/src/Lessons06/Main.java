fb356a7d4b31cbc904916c5a971ebf24842d05e6
Denis Novicov <dema28ster@gmail.com>
2025-02-14 03:24:22 +0100

    Lessons5

---
+++ MyFirstJavaProject/src/Lessons06/Main.java
@@ -0,0 +1,17 @@
+package Lessons06;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello, world!";
+        System.out.println(str.substring(7));
+        System.out.println(str.substring(0, 4));
+        System.out.println(str.substring(str.length() - 3));
+        System.out.println(str.charAt(5));
+
+        System.out.println(str.equals("Hello, world!"));
+
+
+        System.out.println(str);
+
+    }
+}
