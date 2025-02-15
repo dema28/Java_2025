822006729129eed496b74426f7e9b7ac9433b955
Denis Novicov <dema28ster@gmail.com>
2025-02-15 10:13:10 +0100

    Review01

---
+++ MyJavaProject/src/main/java/Novicov/Review/review01.java
@@ -0,0 +1,43 @@
+package Novicov.Review;
+//
+//public class review01 {
+//    public static void main(String[] args) {
+//     int a = 2;
+//     int b = 2;
+//     int c = 3;
+//
+//     a = 5;
+//
+//
+//     a = a + 1;
+//     System.out.println((a + b) * c);
+//     System.out.print("2 + 2 = ");
+//     System.out.println(2 + 2);
+//
+//     System.out.println("\n\nThe end\t");
+
+//}
+
+
+public class review01 {
+  public static void main(String[] args) {
+
+   int a = 9;
+   int b = 2;
+   int c = 2;
+   double two  = 2.0;
+
+   System.out.println((double)a  / b * c);
+   System.out.println(a / b);
+   System.out.println(a % b);
+
+   System.out.println(9 /2);
+   System.out.println(4 * 2);
+   System.out.println(9.0 / 2);
+   System.out.println(9.0 / 2 * 2);
+   System.out.println(9 / 2.0 * 2);
+   System.out.println(9 / 2 * 2.0);
+
+
+ }
+}
