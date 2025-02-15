bb735242aad3311275c13413074900774b70b77c
Denis Novicov <dema28ster@gmail.com>
2025-02-15 10:00:53 +0100

    HomeWork01

---
+++ MyJavaProject/src/main/java/Novicov/HomeWork01/HW_01.java
@@ -0,0 +1,17 @@
+package Novicov.HomeWork01;
+
+/**
+ * Написать программу, которая будет печатать/выводить следующий текст:
+ * 3
+ * 2
+ * 1
+ * Поехали!!!
+ */
+public class HW_01 {
+    public static void main(String[] args) {
+        System.out.println(3);
+        System.out.println(2);
+        System.out.println(1);
+        System.out.println("Поехали!!!");
+    }
+}
---
+++ MyJavaProject/src/main/java/Novicov/HomeWork01/HW_03.java
@@ -0,0 +1,10 @@
+package Novicov.HomeWork01;
+
+/**
+ * Вывести/напечатать смайлик (не :) , а настоящий смайлик одним символом).
+ */
+public class HW_03 {
+    public static void main(String[] args) {
+        System.out.println("Я улыбаюсь: \uD83D\uDE00");
+    }
+}
