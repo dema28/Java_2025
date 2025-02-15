ade88789c1962df9dc6e345efb126f0fb271a42a
Denis Novicov <dema28ster@gmail.com>
2025-02-15 10:03:32 +0100

    HomeWork04

---
+++ MyJavaProject/src/main/java/Novicov/HomeWork04/HW04.java
@@ -0,0 +1,11 @@
+package Novicov.HomeWork04;
+/*
+Необходимо вывести числа от 0 до 15
+ */
+public class HW04 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 15; i++) {
+            System.out.println(i);
+        }
+    }
+}
