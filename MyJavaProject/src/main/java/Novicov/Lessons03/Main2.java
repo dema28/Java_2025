c034a5857e662ef24ec9f2beebd83b3cda359e55
Denis Novicov <dema28ster@gmail.com>
2025-02-15 10:10:50 +0100

    Lessons03

---
+++ MyJavaProject/src/main/java/Novicov/Lessons03/Main2.java
@@ -0,0 +1,17 @@
+package Novicov.Lessons03;
+
+public class Main2 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 3;
+
+        int z = x *2 - y;
+
+        if (z > 20) {
+            System.out.println("z > 20");
+        } else {
+            System.out.println("z <= 20");
+        }
+
+    }
+}
