ffcab76e4226af2b273e1a0557742d3b984df428
Denis Novicov <dema28ster@gmail.com>
2025-02-15 10:12:35 +0100

    Lessons06

---
+++ MyJavaProject/src/main/java/Novicov/Lessons06/Main.java
@@ -0,0 +1,24 @@
+package Novicov.Lessons06;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello, world!";
+        System.out.println(str.substring(7));
+        System.out.println(str.substring(0, 4));
+        System.out.println(str.substring(str.length() - 3));
+        System.out.println(str.charAt(5));
+
+        System.out.println(str.equals("Hello, world!"));
+        System.out.println(str.equalsIgnoreCase("Hello, WorLd!"));
+
+        System.out.println(str.toString());
+
+
+        System.out.println(str.compareTo("AAA"));
+
+
+
+        System.out.println(str);
+
+    }
+}
