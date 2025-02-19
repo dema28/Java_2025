c64a809bd596f6b94db075d2e8c969f4fb133a73
Denis Novicov <dema28ster@gmail.com>
2025-02-19 05:08:41 +0100

    Lessons07

---
+++ MyJavaProject/src/main/java/Novicov/Lessons07/Main.java
@@ -0,0 +1,29 @@
+package Novicov.Lessons07;
+
+public class Main {
+
+    public static void printHi () {
+        System.out.println("hi");
+    }
+
+    public static void printALL () {
+        System.out.print("hi");
+    }
+
+    public static void printText (String str) {
+        System.out.print(str);
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+//        String str = "123";
+//        System.out.println(str.replace("2","+++"));
+
+        printHi();
+        printALL();
+        printText("hello");
+    }
+}
